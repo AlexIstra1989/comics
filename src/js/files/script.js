1e203bd968c6437816eaa787bc7e9e94fd26f185
AlexIstra1989 <alex888fcsm@gmail.com>
2022-11-08 20:16:12 +0300

    added popup

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -2,3 +2,16 @@
 import { isMobile } from "./functions.js";
 // Подключение списка активных модулей
 import { flsModules } from "./modules.js";
+
+$(document).ready(function() {
+	$('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
+		disableOn: 700,
+		type: 'iframe',
+		mainClass: 'mfp-fade',
+		removalDelay: 160,
+		preloader: false,
+  alignYop: true,
+		fixedContentPos: false,
+
+	});
+});
